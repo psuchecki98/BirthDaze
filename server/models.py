34151c2885e05646d561fcce97b0820537d66424
Patryk Suchecki <sucheckipatryk@gmail.com>
2023-07-28 15:27:24 -0400

    Added to models.py - Created Birthday, Notification, Friend, Tag, and BirthdayTag models

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,8 +11,13 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+#-----------------------------------------
+#Add Rules and Validations
+#Validations for username, email, password
+#-----------------------------------------
+
 class User(db.Model, SerializerMixin):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), unique=True)
@@ -25,5 +30,59 @@ class User(db.Model, SerializerMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+
+class Birthday(db.Model, SerializerMixin):
+    __tablename__ = 'birthdays'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='birthdays')
+
+
+class Notification(db.Model, SerializerMixin):
+    __tablename__ = 'notifications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, default= datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='notifications')
+
+
+class Friend(db.Model, SerializerMixin):
+    __tablename__ = 'friends'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    email = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='friends')
+
+
+class Tag(db.Model, SerializerMixin):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='tags')
+
+    #Many to Many relationship between tags and birthdays
+    birthdays = db.relationship('Birthday', secondary= 'birthday_tags', backref='tags')
+
+
+class BirthdayTag(db.Model, SerializerMixin):
+    __tablename__ = 'birthday_tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    birthday_id = db.Column(db.Integer, db.ForeignKey('birthdays.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
+
 
 
